6bc96066853e121ab6d85f954163dadc68abf447
Asif Nawaz <asifnawaz5926@gmail.com>
2020-11-27 17:59:08 +0600

    Completed Basic CSS structure and movies and tv series tab in ftpserver

---
+++ FTPServer/controller/ftpsite.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const moviesModel = require.main.require("./models/moviesModel");
+const tvseriesModel = require.main.require("./models/tvseriesModel");
+
+router.get("/", (req,res ) => {
+
+    moviesModel.getByAction(function (results) {
+       var actionMovies=results;
+       moviesModel.getByThriller(function (results) {
+        var thrillerMovies=results;
+        moviesModel.getByComedy(function (results) {
+            var comedyMovies=results;
+            tvseriesModel.getByAction(function (results) {
+                var actionTvSeries=results;
+                tvseriesModel.getByThriller(function (results) {
+                    var thrillerTvSeries=results;
+                    tvseriesModel.getByComedy(function (results) {
+                        var comedyTvSeries=results;
+                        res.render("ftpsite/index",{actionMovies : actionMovies, thrillerMovies : thrillerMovies, comedyMovies : comedyMovies,actionTvSeries : actionTvSeries,thrillerTvSeries : thrillerTvSeries,comedyTvSeries : comedyTvSeries });
+                        });
+                    });
+             
+                });
+         
+            });
+ 
+         });
+
+      });
+   
+});
+
+  
+module.exports = router;
